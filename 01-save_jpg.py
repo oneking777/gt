bc70cb3c6e7b8e97cdf19eaedb34e9d1a196cfb3
root <guoandadada@163.com>
2019-07-23 19:36:34 +0800

    01

---
+++ 01-save_jpg.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+
+# 发送请求
+resp = requests.get("https://www.sina.com.cn/")
+
+# print(resp.content.decode())
+# print("-"*50)
+# print(resp.text)
+
+# assert resp.status_code == 200
+
+
+
+
+
+
+
+
